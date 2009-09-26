854edc43e56e6611459e1b250bdd410ccad678b4
Leon Handreke <leonbrussels@gmail.com>
2009-09-26 18:28:13 +0200

    Initial commit

---
+++ Classes/fosdemAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  fosdemAppDelegate.h
+//  fosdem
+//
+//  Created by Leon on 9/26/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface fosdemAppDelegate : NSObject <UIApplicationDelegate, UITabBarControllerDelegate> {
+    UIWindow *window;
+    UITabBarController *tabBarController;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet UITabBarController *tabBarController;
+
+@end
---
+++ Classes/fosdemAppDelegate.m
@@ -0,0 +1,45 @@
+//
+//  fosdemAppDelegate.m
+//  fosdem
+//
+//  Created by Leon on 9/26/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import "fosdemAppDelegate.h"
+
+
+@implementation fosdemAppDelegate
+
+@synthesize window;
+@synthesize tabBarController;
+
+
+- (void)applicationDidFinishLaunching:(UIApplication *)application {
+    
+    // Add the tab bar controller's current view as a subview of the window
+    [window addSubview:tabBarController.view];
+}
+
+
+/*
+// Optional UITabBarControllerDelegate method
+- (void)tabBarController:(UITabBarController *)tabBarController didSelectViewController:(UIViewController *)viewController {
+}
+*/
+
+/*
+// Optional UITabBarControllerDelegate method
+- (void)tabBarController:(UITabBarController *)tabBarController didEndCustomizingViewControllers:(NSArray *)viewControllers changed:(BOOL)changed {
+}
+*/
+
+
+- (void)dealloc {
+    [tabBarController release];
+    [window release];
+    [super dealloc];
+}
+
+@end
+
