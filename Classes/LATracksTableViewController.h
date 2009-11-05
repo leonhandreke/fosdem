466627576976dc9edaee2f9ffc981c3f8aeb2f5c
Adam Ziolkowski <adam@landasoftware.com>
2009-11-05 10:59:16 +0100

    Adding source files for Tracks table view.

---
+++ Classes/LATracksTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  LATracksTableViewController.h
+//  fosdem
+//
+//  Created by Adam Ziolkowski on 05/11/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LAEventDatabase.h"
+#import "LAEvent.h"
+@interface LATracksTableViewController : UITableViewController {
+
+}
+
+@end
