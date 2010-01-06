33c5e93936076156c7d94efad4534fb1ecb5f771
Leon Handreke <leonbrussels@gmail.com>
2010-01-06 12:16:39 +0100

    Added a state-of-the-art beerware license.

--- Classes/LAEvent.m
+++ Classes/LAEvent.m
@@ -1,10 +1,12 @@
-//
-//  LAEvent.m
-//  fosdem
-//
-//  Created by Leon on 9/26/09.
-//  Copyright 2009 __MyCompanyName__. All rights reserved.
-//
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * Adam Ziolkowski <adam@landasoftware.com> and Leon Handreke <leon.handreke@gmail.com>
+ * wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy us a beer in return.
+ * ----------------------------------------------------------------------------
+ */
 
 #import "LAEvent.h"
 
--- Classes/LAStarredTableViewController.h
+++ Classes/LAStarredTableViewController.h
@@ -1,10 +1,12 @@
-//
-//  LAStarredTableViewController.h
-//  fosdem
-//
-//  Created by Leon on 11/24/09.
-//  Copyright 2009 __MyCompanyName__. All rights reserved.
-//
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * Adam Ziolkowski <adam@landasoftware.com> and Leon Handreke <leon.handreke@gmail.com>
+ * wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy us a beer in return.
+ * ----------------------------------------------------------------------------
+ */
 
 #import <UIKit/UIKit.h>
 
